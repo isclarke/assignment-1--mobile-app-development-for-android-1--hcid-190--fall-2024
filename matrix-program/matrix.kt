e14650db1d581f4ed54c9aca8d383e949bcb6b9a
isclarke <156957402+isclarke@users.noreply.github.com>
2024-10-08 18:01:32 -0400

    add syntax to read num

--- matrix-program/matrix.kt
+++ matrix-program/matrix.kt
@@ -1,1 +1,13 @@
 import java.util.Scanner
+
+fun main() {
+  Matrix()
+}
+
+class Matrix {
+  init {
+    println("Enter a positive number")
+    val scanner = Scanner(System.`in`)
+    var number: Int = scanner.nextInt()
+  }
+}
